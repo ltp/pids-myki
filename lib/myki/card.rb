93ed1c0681ae7f32da17d86a4a1b07b17e52c305
Luke Poskitt <luke.poskitt@gmail.com>
2016-11-04 14:59:50 +1100

    Corrected classing.

--- lib/myki/card.rb
+++ lib/myki/card.rb
@@ -1,13 +1,22 @@
-class Card
+class Myki
 
-  attr_reader :id, :link, :holder, :money, :pass
+  class Card
 
-  def initialize( params = {} )
-    @id = params[:id]
-    @link = params[:link]
-    @holder = params[:holder]
-    @money = params[:money]
-    @pass = params[:pass]
+    attr_reader :id, :link, :holder, :money, :pass
+
+    def initialize( params = {} )
+      @id = params[:id]
+      @link = params[:link]
+      @holder = params[:holder]
+      @money = params[:money]
+      @pass = params[:pass]
+      @agent = params[:agent]
+    end
+
+    def _get_card_details
+      #page = @agent.get( 
+    end
+
   end
 
 end
